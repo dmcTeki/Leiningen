36ae7e60816869843365e91e1adbf0ca755671b3
Davi Marques Cavalcanti <davi.marques@dcx.ufpb.br>
2025-04-25 19:43:25 -0300

    Update core.clj

--- src/caca_palavras/core.clj
+++ src/caca_palavras/core.clj
@@ -1,50 +1,90 @@
 (ns caca-palavras.core
   (:require [clojure.string :as str]))
 
-(def todas-palavras ["LIVRO" "CADERNO" "LAPIS" "BORRACHA" "MOCHILA" "TESOURA" "CANETA" "QUADRO" "GIZ" 
-                    "PAPEL" "FOLHA" "LIVRARIA" "ESCOLA" "PROFESSOR" "ALUNO" "AULA" "TURMA" "EXAME"
-                    "NOTA" "TAREFA" "ATIVIDADE" "DICIONARIO"])
-
-(def palavras-escondidas (take 4 (shuffle todas-palavras)))
+(def todas-palavras ["LIVRO" "CADERNO" "LAPIS" "BORRACHA" "MOCHILA" "TESOURA" "CANETA" "QUADRO" "GIZ"
+                     "PAPEL" "FOLHA" "LIVRARIA" "ESCOLA" "PROFESSOR" "ALUNO" "AULA" "TURMA" "EXAME"
+                     "NOTA" "TAREFA" "ATIVIDADE" "DICIONARIO"])
 
 (def alfabeto "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-(defn gerar-linha-aleatoria [tamanho]
-  (vec (repeatedly tamanho #(str (rand-nth alfabeto)))))
-
-(defn pode-inserir-na-linha? [linha palavra pos]
-  (let [linha-str (str/join "" linha)
-        palavra-up (str/upper-case palavra)
-        tamanho-linha (count linha-str)]
-    (and (<= (+ pos (count palavra-up)) tamanho-linha)
-         (not-any? #(= palavra-up (subs linha-str % (+ % (count palavra-up)))) (range (inc (- tamanho-linha (count palavra-up))))))))
-
-(defn inserir-palavra-na-linha [linha palavra pos]
-  (let [parte1 (subvec linha 0 pos)
-        parte2 (vec (map str (seq palavra)))
-        parte3 (subvec linha (+ pos (count palavra)))]
-    (into [] (concat parte1 parte2 parte3)))) ;
-
-(defn inserir-palavra-na-grade [grade palavra]
-  (let [linhas (shuffle (range (count grade)))
-        palavra-up (str/upper-case palavra)]
-    (loop [[linha-idx & restantes] linhas]
-      (if (nil? linha-idx)
-        (do
-          (println "Não foi possível inserir a palavra:" palavra-up)
-          grade)
-        (let [linha (grade linha-idx)
-              max-pos (- (count linha) (count palavra-up))
-              posicoes (shuffle (range (inc max-pos)))]
-          (if-let [pos (first (filter #(pode-inserir-na-linha? linha palavra-up %) posicoes))]
-            (assoc grade linha-idx (inserir-palavra-na-linha linha palavra-up pos))
-            (recur restantes)))))))
+(def palavras-escondidas (atom [])) ; agora é um atom
 
 (defn gerar-grade [tamanho]
-  (vec (repeatedly tamanho #(gerar-linha-aleatoria tamanho))))
+  (vec (repeat tamanho (vec (repeat tamanho nil)))))
+
+(defn pode-inserir? [grade palavra linha coluna delta-linha delta-coluna]
+  (let [tamanho (count grade)
+        posicoes (for [i (range (count palavra))]
+                   [(+ linha (* i delta-linha)) (+ coluna (* i delta-coluna))])]
+    (every? (fn [[l c]]
+              (and (< l tamanho)
+                   (< c tamanho)
+                   (nil? (get-in grade [l c]))))
+            posicoes)))
+
+(defn inserir-em-grade [grade palavra linha coluna delta-linha delta-coluna]
+  (let [posicoes (for [i (range (count palavra))]
+                   [(+ linha (* i delta-linha)) (+ coluna (* i delta-coluna))])]
+    (reduce (fn [g [[l c] ch]]
+              (assoc-in g [l c] (str ch)))
+            grade
+            (map vector posicoes palavra))))
+
+(defn inserir-horizontal [grade palavra linha coluna]
+  (let [tamanho (count grade)]
+    (if (<= (+ coluna (count palavra)) tamanho)
+      (if (pode-inserir? grade palavra linha coluna 0 1)
+        (inserir-em-grade grade palavra linha coluna 0 1)
+        nil)
+      nil)))
+
+(defn inserir-vertical [grade palavra linha coluna]
+  (let [tamanho (count grade)]
+    (if (<= (+ linha (count palavra)) tamanho)
+      (if (pode-inserir? grade palavra linha coluna 1 0)
+        (inserir-em-grade grade palavra linha coluna 1 0)
+        nil)
+      nil)))
+
+(defn inserir-palavra-na-grade [grade palavra]
+  (let [palavra-up (str/upper-case palavra)
+        tamanho (count grade)
+        direcoes [:horizontal :vertical]]
+    (loop [tentativas 0]
+      (if (>= tentativas 100)
+        (do
+          (println "Não foi possível inserir a palavra:" palavra-up)
+          nil)
+        (let [linha (rand-int tamanho)
+              coluna (rand-int tamanho)
+              direcao (rand-nth direcoes)
+              nova-grade
+              (case direcao
+                :horizontal (inserir-horizontal grade palavra-up linha coluna)
+                :vertical (inserir-vertical grade palavra-up linha coluna))]
+          (if nova-grade
+            nova-grade
+            (recur (inc tentativas))))))))
+
+(defn preencher-grade [grade]
+  (mapv (fn [linha]
+          (mapv #(if (nil? %) (str (rand-nth alfabeto)) %) linha))
+        grade))
 
 (defn gerar-grade-com-palavras [tamanho palavras]
-  (reduce inserir-palavra-na-grade (gerar-grade tamanho) palavras))
+  (loop [grade (gerar-grade tamanho)
+         palavras-a-inserir palavras
+         palavras-inseridas []]
+    (if (or (empty? palavras-a-inserir)
+            (>= (count palavras-inseridas) 4))
+      (do
+        (reset! palavras-escondidas palavras-inseridas)
+        (preencher-grade grade))
+      (let [palavra (first palavras-a-inserir)
+            nova-grade (inserir-palavra-na-grade grade palavra)]
+        (if (nil? nova-grade)
+          (recur grade (rest palavras-a-inserir) palavras-inseridas)
+          (recur nova-grade (rest palavras-a-inserir) (conj palavras-inseridas palavra)))))))
 
 (defn exibir-grade [grade]
   (doseq [linha grade]
@@ -64,31 +104,14 @@
   (let [transposta (apply map vector grade)]
     (some #(contem-substring-exata? % palavra) transposta)))
 
-(defn palavra-diagonal? [grade palavra]
-  (let [n (count grade)
-        palavra-up (str/upper-case palavra)
-        len (count palavra-up)
-        diagonais (for [i (range n)
-                        j (range n)
-                        :when (and (<= (+ i len) n)
-                                   (<= (+ j len) n))]
-                    [(for [k (range len)] (get-in grade [(+ i k) (+ j k)])) ; ↘
-                     (for [k (range len)] (get-in grade [(+ i k) (- j k)])) ; ↙
-                     (for [k (range len)] (get-in grade [(- i k) (+ j k)])) ; ↗
-                     (for [k (range len)] (get-in grade [(- i k) (- j k)])) ; ↖
-                     ])]
-    (some #(= (vec (map str palavra-up)) %)
-          (apply concat diagonais))))
-
 (defn encontrou-palavra? [grade palavra]
   (or (palavra-horizontal? grade palavra)
-      (palavra-vertical? grade palavra)
-      (palavra-diagonal? grade palavra)))
+      (palavra-vertical? grade palavra))) ; por enquanto, só horizontal e vertical
 
 (defn jogar []
   (let [tamanho 10
-        grade (gerar-grade-com-palavras tamanho palavras-escondidas)
-        palavras-restantes (atom (set palavras-escondidas))]
+        grade (gerar-grade-com-palavras tamanho (shuffle todas-palavras))
+        palavras-restantes (atom (set @palavras-escondidas))]
     (println "Bem-vindo ao jogo de Caça-Palavras!")
     (println "Tente encontrar todas as palavras escondidas na grade.")
     (println)
